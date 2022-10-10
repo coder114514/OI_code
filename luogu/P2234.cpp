abcb3d9559b7616b61af992ab37b0ee40d894db8
coder114514 <98397499+coder114514@users.noreply.github.com>
2022-10-10 22:56:53 +0800

    Update P2234.cpp

--- luogu/P2234.cpp
+++ luogu/P2234.cpp
@@ -1,58 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-const int N=32767+10;
-const int INF=1<<28;
+int a,n,ans;
+set<int> s;
 
-struct Val
-{
-    int num,day;
-    bool operator<(const Val&o) const
-    {
-        if(num==o.num)return day<o.day;
-        else return num<o.num;
-    }
-} x[N];
-
-int n;
-
-int check(int pos)
-{
-    int bef=-INF,aft=INF;
-    for(int i=pos-1;i>=1;i--)
-    {
-        if(x[i].day<x[pos].day)
-        {
-            bef=x[i].num;
-            break;
+int main(){
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        scanf("%d",&a);
+        if(s.empty()){
+            ans+=a;
+            s.insert(a);
+            continue;
         }
-    }
-    for(int i=pos+1;i<=n;i++)
-    {
-        if(x[i].day<x[pos].day)
-        {
-            aft=x[i].num;
-            break;
+        auto it=s.lower_bound(a);
+        if(it==s.end()){
+            ans+=a-*prev(it);
+        }else if(it==s.begin()){
+            ans+=*it-a;
+        }else{
+            ans+=min(*it-a,a-*prev(it));
         }
+        s.insert(a);
     }
-    if(bef==-INF&&aft==INF)return x[pos].num;
-    return min(x[pos].num-bef,aft-x[pos].num);
-}
-
-int main()
-{
-    cin>>n;
-    for(int i=1;i<=n;i++)
-    {
-        x[i].day=i;
-        cin>>x[i].num;
-    }
-    sort(x+1,x+n+1);
-    int ans=0;
-    for(int i=1;i<=n;i++)
-    {
-        ans+=check(i);
-    }
-    cout<<ans<<endl;
+    printf("%d\n",ans);
     return 0;
 }
